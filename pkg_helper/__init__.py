61e7fb064395116cdb6703e354f9709cfd6537e0
Moody Liu <mooodyhunter@outlook.com>
2024-05-07 18:51:41 +0100

    pkg: update helper

--- pkg_helper/__init__.py
+++ pkg_helper/__init__.py
@@ -1,9 +1,29 @@
 import os
 from glob import glob
 from pathlib import Path
+from pprint import pprint
 
 import yaml
 from termcolor import colored, cprint
+from termcolor._types import Color
+
+
+class UnsupportedTargetError(Exception):
+    def __init__(self, package: "Package", target: str):
+        self.package = package
+        super().__init__(f"Package {package.basename} does not support target {target}")
+
+
+class InvalidTargetError(Exception):
+    def __init__(self, target: str):
+        self.target = target
+        super().__init__(f"Invalid target triple: {target}")
+
+
+class PackageNotFoundError(Exception):
+    def __init__(self, package: str):
+        self.package = package
+        super().__init__(f"Package {package} not found.")
 
 
 class Config:
@@ -11,18 +31,6 @@ class Config:
 
 
 CONFIG = Config()
-
-
-def try_find_package(hint: str, *, arch=None) -> str | list[str]:
-    # try to find the package with the exact name
-    package = os.path.join("packages", hint)
-    if package and os.path.isdir(package):
-        return Path(package).name
-
-    # if still not found, try globbing
-    candidates = glob(f"packages/{arch or ""}*{hint}*/")
-    candidates = [Path(candidate).name for candidate in candidates]
-    return candidates[0] if len(candidates) == 1 else candidates
 
 
 def sanitise_triple(target: str) -> str:
@@ -33,7 +41,7 @@ def sanitise_triple(target: str) -> str:
     return target
 
 
-def colored_list(l, color: str):
+def colored_list(l, color: Color | None):
     return [colored(e, color, attrs=['bold']) for e in l] if l else ["-"]
 
 
@@ -47,38 +55,137 @@ class YamlStore:
         return self.store[path]
 
 
-class Package:
-    def __init__(self, name):
-        self._fullname = name
-        self.basename = name
-        self.triples = set()
-        for target in ALL_TARGET_TRIPLE:
-            prefix = target + "-"
-            if name.startswith(prefix):
-                self.triples.add(target)
-                self.basename = name[len(prefix):]
-                break
-        else:
-            self.triples = ANY_TARGET_TRIPLE
+def extract_package_info(pkgname: str) -> tuple[str, str]:
+    """Extract the target triple and package base name from a package name.
 
-        self.yamlstore = {}
+    Args:
+        pkgname (str): The package name, e.g. "x86_64-mos-foo".
+
+    Returns:
+        tuple[str, str]: A tuple containing (target_triple, package_basename)
+    """
+    for target in ALL_TARGET_TRIPLE:
+        if pkgname.startswith(target + "-"):
+            return target, pkgname[len(target) + 1:]
+    return "any", pkgname
+
+
+def get_solid_package_by_fullname(fullname: str) -> "SolidPackage":
+    triple, basename = extract_package_info(fullname)
+    for pkg in PACKAGES.values():
+        if pkg.basename == basename:
+            if triple in pkg.triples:
+                return SolidPackage(pkg, triple)
+            else:
+                raise UnsupportedTargetError(pkg, triple)
+
+    raise PackageNotFoundError(fullname)
+
+
+def try_find_package(hint: str, *, target: str = "") -> str | list[str]:
+    # try to find the package with the exact name
+    package = os.path.join("packages", hint)
+    if package and os.path.isdir(package):
+        return Path(package).name
+
+    # if still not found, try globbing
+    candidates = glob(f"packages/{target}*{hint}*/")
+    candidates = [Path(candidate).name for candidate in candidates]
+    return candidates[0] if len(candidates) == 1 else candidates
+
+
+class SolidPackage:
+    def __init__(self, package: "Package", target: str, *, allow_nonsupport=False):
+        self.basepkg = package
+        self.triple = target
+        self._allow_nonsupport = allow_nonsupport
+        if target not in package.triples and not allow_nonsupport:
+            raise UnsupportedTargetError(package, target)
+
+    def __repr__(self):
+        return f"SolidPackage({self.basepkg.basename} for {self.triple})"
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, SolidPackage):
+            return False
+        return self.basepkg.basename == value.basepkg.basename and self.triple == value.triple
+
+    def __hash__(self) -> int:
+        return hash((self.basepkg.basename, self.triple))
+
+    @property
+    def pkgdir(self):
+        return self.basepkg.pkgdir(self.triple, allow_nonsupport=self._allow_nonsupport)
+
+    @property
+    def basename(self):
+        return self.basepkg.basename
+
+    @property
+    def fullname(self):
+        return self.basepkg.target_fullname(self.triple)
+
+    @property
+    def depends(self):
+        return self.basepkg.target_depends[self.triple]
+
+    @property
+    def makedepends(self):
+        return self.basepkg.target_makedepends[self.triple]
+
+    @property
+    def rebuild(self):
+        return self.basepkg.target_rebuild[self.triple]
+
+
+class Package:
+    def __init__(self, basename: str, triples: set[str]):
+        if "any" in triples:
+            if len(triples) > 1:
+                raise InvalidTargetError(f"{", ".join(triples)}")  # if 'any' is present, it should be the only element
+
+        self.basename: str = basename
+        self.triples: set[str] = triples
+        self.target_depends: dict[str, list[SolidPackage]] = {}
+        self.target_makedepends: dict[str, list[SolidPackage]] = {}
+        self.target_rebuild: dict[str, list[SolidPackage]] = {}
+
+    def __repr__(self):
+        return f'Package({self.basename} for {", ".join(self.triples)})'
+
+    def _do_resolve_all_depends(self):
+        for triple in self.triples:
+            yaml_data = YAML_STORE.get(self.get_yaml_path(triple))
+
+            # resolve depends
+            self.target_depends[triple] = [get_solid_package_by_fullname(d) for d in yaml_data.get("repo_depends", [])]
+            self.target_makedepends[triple] = [get_solid_package_by_fullname(d) for d in yaml_data.get("repo_makedepends", [])]
+            self.target_rebuild[triple] = [get_solid_package_by_fullname(e.get("pkgbase")) for e in yaml_data.get("update_on_build", [])]
 
     def _do_check_triple(self, triple: str):
         if triple not in ALL_TARGET_TRIPLE:
-            raise ValueError(f"Invalid target triple: {triple}")
+            raise InvalidTargetError(triple)
 
         if triple not in self.triples:
-            raise ValueError(f"Package {self.basename} does not support target {triple}")
+            raise UnsupportedTargetError(self, triple)
 
     def _do_get_filepath(self, filename: str, triple: str, allow_nonsupport=False):
-        if self.triples == ANY_TARGET_TRIPLE:
+        if ANY_TARGET in self.triples:
             return f"packages/{self.basename}/{filename}"
 
         if not allow_nonsupport:
             self._do_check_triple(triple)
         return f"packages/{triple}-{self.basename}/{filename}"
 
-    def get_package_dir(self, triple: str, allow_nonsupport=False):
+    def isany(self):
+        return ANY_TARGET in self.triples
+
+    def target_fullname(self, target: str):
+        if target == 'any':
+            return self.basename
+        return f"{target}-{self.basename}"
+
+    def pkgdir(self, triple: str, allow_nonsupport=False):
         return self._do_get_filepath("", triple, allow_nonsupport)
 
     def get_yaml_path(self, triple: str, allow_nonsupport=False):
@@ -87,32 +194,29 @@ class Package:
     def get_pkgbuild_path(self, triple: str, allow_nonsupport=False):
         return self._do_get_filepath("PKGBUILD", triple, allow_nonsupport)
 
-    def get_friends_name(self, friend: "Package", triple: str, full_name=False):
+    def supports_target(self, triple: str):
+        return triple in self.triples
+
+    def get_friends_name(self, friend: SolidPackage, triple: str, full_name=False, pretty=True):
         if CONFIG.print_full_pkgname or full_name:
-            return friend._fullname
+            return friend.basepkg.target_fullname(triple)
 
-        if triple in friend.triples:
-            return friend.basename
+        if triple == friend.triple:
+            return friend.basepkg.basename
 
-        return friend.basename + colored(f" ({', '.join(friend.triples)})", "red", attrs=["bold"])
+        return friend.basepkg.basename + colored(f" ({friend.triple})", "red", attrs=["bold"])
 
-    def get_friend_names(self, friends: list["Package"], triple: str, full_name=False):
+    def get_friend_names(self, friends: list[SolidPackage], triple: str, full_name=False):
         return [self.get_friends_name(friend, triple, full_name) for friend in friends]
 
-    def get_depends(self, triple=None, full_name=False):
-        yaml_data = YAML_STORE.get(self.get_yaml_path(triple))
-        depends = self.get_friend_names([Package(dep) for dep in yaml_data.get("repo_depends", [])], triple, full_name)
-        return depends
+    def get_depends(self, triple: str) -> list[SolidPackage]:
+        return self.target_depends[triple] or []
 
-    def get_makedepends(self, triple=None, full_name=False):
-        yaml_data = YAML_STORE.get(self.get_yaml_path(triple))
-        makedepends = self.get_friend_names([Package(dep) for dep in yaml_data.get("repo_makedepends", [])], triple, full_name)
-        return makedepends
+    def get_makedepends(self, triple: str) -> list[SolidPackage]:
+        return self.target_makedepends[triple] or []
 
-    def get_rebuild(self, triple=None, full_name=False):
-        yaml_data = YAML_STORE.get(self.get_yaml_path(triple))
-        rebuilds = [self.get_friends_name(Package(e.get("pkgbase")), triple, full_name) for e in yaml_data.get("update_on_build", [])]
-        return rebuilds
+    def get_rebuild(self, triple: str) -> list[SolidPackage]:
+        return self.target_rebuild[triple] or []
 
     def print_info(self, *, selected_target=None, has_deps=False, has_makedeps=False, has_rebuild=False):
         cprint(self.basename, "green", attrs=["bold"], end=" ")
@@ -126,12 +230,16 @@ class Package:
             if selected_target and triple != selected_target:
                 continue
 
-            deps = self.get_depends(triple) if has_deps else None
-            makedeps = self.get_makedepends(triple) if has_makedeps else None
-            rebuild_by = self.get_rebuild(triple) if has_rebuild else None
+            deps = self.get_depends(triple) if has_deps else []
+            makedeps = self.get_makedepends(triple) if has_makedeps else []
+            rebuild_by = self.get_rebuild(triple) if has_rebuild else []
 
             if not (deps or makedeps or rebuild_by):
                 continue
+
+            deps = self.get_friend_names(deps, triple)
+            makedeps = self.get_friend_names(makedeps, triple)
+            rebuild_by = self.get_friend_names(rebuild_by, triple)
 
             # only need to print the target triple header if:
             # 1. the target is not 'any'
@@ -155,8 +263,8 @@ class Package:
 
 
 ALL_ARCH = ["x86_64", "riscv64"]
-ALL_TARGET_TRIPLE = [f"{arch}-mos" for arch in ALL_ARCH] + [f"{arch}-elf" for arch in ALL_ARCH]
-ANY_TARGET_TRIPLE = set(['any'])
+ALL_TARGET_TRIPLE = [f"{arch}-mos" for arch in ALL_ARCH] + [f"{arch}-elf" for arch in ALL_ARCH] + ["any"]
+ANY_TARGET = 'any'
 
 PACKAGES: dict[str, Package] = {}
 
@@ -167,14 +275,20 @@ def initialise():
     PACKAGES.clear()
     CONFIG.print_full_pkgname = False
 
-    names = [dir for dir in os.listdir("packages") if os.path.isdir("packages/" + dir)]
-    names.sort()
+    tmp_storage = {}
+    for pkgname in sorted(os.listdir("packages")):
+        if not os.path.isdir(f"packages/{pkgname}"):
+            continue
 
-    for pkgname in names:
-        p = Package(pkgname)
+        if not os.path.isfile(f"packages/{pkgname}/lilac.yaml") or not os.path.isfile(f"packages/{pkgname}/PKGBUILD"):
+            cprint(f'missing lilac.yaml or PKGBUILD in {pkgname}, skipping', "yellow")
+            continue
 
-        if p.basename in PACKAGES:
-            assert PACKAGES[p.basename].triples != ANY_TARGET_TRIPLE
-            PACKAGES[p.basename].triples |= p.triples
-        else:
-            PACKAGES[p.basename] = p
+        triple, basename = extract_package_info(pkgname)
+        tmp_storage[basename] = tmp_storage.get(basename, set()) | {triple}
+
+    for basename, triples in tmp_storage.items():
+        PACKAGES[basename] = Package(basename, triples)
+
+    for basename in PACKAGES:
+        PACKAGES[basename]._do_resolve_all_depends()
--- pkg_helper/porting.py
+++ pkg_helper/porting.py
@@ -1,25 +1,255 @@
 
+from abc import ABC, abstractmethod
 from enum import Enum
+import os
+import random
+import shutil
+from time import sleep
+
+from termcolor import colored, cprint
+
+from pkg_helper import PACKAGES, Package, PackageNotFoundError, SolidPackage
 
 
-class ActionType(Enum):
-    MAKE_DIR = "Make-Package-Dir"
-    COPY_FILE = "Copy-File"
-    MODIFY_DEPS = "Modify-Deps"
-    MODIFY_MAKEDEPS = "Modify-MakeDeps"
-    MODIFY_REBUILD = "Modify-Rebuild"
-    PATCH_FILE = "Patch-File"
-    RENAME_FILE = "Rename-File"
-    UPDPKGSUMS = "Update-PKGSUMS"
+class ExecutionError(Exception):
+    def __init__(self, message: str):
+        self.message = message
 
 
-class Action:
-    action: ActionType
-    src: str
-    dst: str
+class AlternativeWorkDir:
+    def __init__(self, path: str):
+        self.saved = os.getcwd()
+        os.chdir(path)
 
-    def __init__(self, action: ActionType, src: str, dst: str, file: str = None):
-        self.action = action
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.chdir(self.saved)
+
+
+class DependencyList:
+    def __init__(self, depends: list[SolidPackage], makedepends: list[SolidPackage], rebuild: list[SolidPackage]):
+        self.depends = depends
+        self.makedepends = makedepends
+        self.rebuild = rebuild
+
+    def is_empty(self):
+        return not self.depends and not self.makedepends and not self.rebuild
+
+
+class PortingExecutionContext:
+    def __init__(self, srcpkg: SolidPackage, target_triple: str):
+        self.srcpkg = srcpkg
+        self.dstpkg = SolidPackage(srcpkg.basepkg, target_triple, allow_nonsupport=True)
+        self._basepkg = srcpkg.basepkg
+        self.source_triple = self.srcpkg.triple
+        self.target_triple = self.dstpkg.triple
+
+    @property
+    def basename(self):
+        return self._basepkg.basename
+
+
+class BaseAction(ABC):
+    def __init__(self, src: str, dst: str, file: str = ""):
         self.src = src
         self.dst = dst
         self.file = file
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.src!r}, {self.dst!r}, {self.file!r})"
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, BaseAction):
+            return False
+        return self.src == value.src and self.dst == value.dst and self.file == value.file
+
+    def __hash__(self) -> int:
+        return hash((self.src, self.dst, self.file))
+
+    @abstractmethod
+    def execute(self, ctx: PortingExecutionContext):
+        raise NotImplementedError
+
+
+class MakeDirAction(BaseAction):
+    def execute(self, ctx: PortingExecutionContext):
+        pkgdir = ctx.dstpkg.pkgdir
+        try:
+            os.mkdir(pkgdir)
+        except FileExistsError as e:
+            dircontent = os.listdir(pkgdir)
+            if dircontent:
+                cprint(f"Directory '{pkgdir}' already exists and is not empty", "red")
+                for item in dircontent:
+                    print(f"  {item}")
+                return False
+            else:
+                pass  # Directory is empty, we can continue
+
+        return True
+
+
+class CopyFileAction(BaseAction):
+    def execute(self, ctx: PortingExecutionContext):
+        try:
+            shutil.copyfile(self.src, self.dst)
+        except Exception as e:
+            raise ExecutionError(f"Error copying '{self.src}' to '{self.dst}': {e}")
+
+        return True
+
+
+class PatchFileAction(BaseAction):
+    def execute(self, ctx: PortingExecutionContext):
+        try:
+            with open(self.file, "r") as f:
+                content = f.read()
+            content = content.replace(self.src, self.dst)
+            with open(self.file, "w") as f:
+                f.write(content)
+        except Exception as e:
+            raise ExecutionError(f"Error patching '{self.src}' to '{self.dst}': {e}")
+
+        return True
+
+
+class RenameFileAction(BaseAction):
+    def execute(self, ctx: PortingExecutionContext):
+        try:
+            os.rename(self.src, self.dst)
+        except Exception as e:
+            cprint(f"Error renaming '{self.src}' to '{self.dst}': {e}", "red")
+            return False
+
+        return True
+
+
+class UpdPkgSumsAction(BaseAction):
+    def execute(self, ctx: PortingExecutionContext):
+        with AlternativeWorkDir(f"packages/{ctx.dstpkg.fullname}/"):
+            result = os.system("updpkgsums 2>/dev/null")
+
+        if result != 0:
+            cprint(f"Error running 'updpkgsums' for '{ctx.dstpkg.fullname}': command returned {result}", "red")
+            return False
+
+        return True
+
+
+class Action(Enum):
+    MAKE_DIR = MakeDirAction
+    COPY_FILE = CopyFileAction
+    PATCH_FILE = PatchFileAction
+    RENAME_FILE = RenameFileAction
+    UPDPKGSUMS = UpdPkgSumsAction
+
+
+class PortDriver:
+    def __init__(self, source_pkg: SolidPackage, target_triple: str) -> None:
+        self.actions: list[BaseAction] = []
+        self.context = PortingExecutionContext(source_pkg, target_triple)
+        self.add_step(Action.MAKE_DIR, "", "", self.context.dstpkg.pkgdir)
+
+    def add_step(self, action: Action, src: str, dst: str, file: str = ""):
+        actionclass = action.value
+        real_action = actionclass(src, dst, file)
+        if not isinstance(real_action, BaseAction):
+            raise ValueError(f"Action {action} is not a valid action")
+
+        self.actions.append(real_action)
+
+    def print_steps(self):
+        cprint("The following actions will be taken:", "green", attrs=["bold"])
+
+        for action in self.actions:
+            self.print_single_step(action)
+
+    def print_single_step(self, action: BaseAction):
+        i = self.actions.index(action)
+        print(f" {i+1:3}: {colored(action.__class__.__name__.removesuffix("Action"), "yellow", attrs=["bold"])}", end=" ")
+        b = False
+        if action.src != "" and action.dst != "":
+            print(f"{colored(action.src, "red")} -> {colored(action.dst, "green")}", end=" ")
+            b = True
+        if action.file:
+            print(f"(in {colored(action.file, "blue")})", end=" ")
+            b = True
+        if b:
+            print()
+        else:
+            print()
+
+    def check_package_deps(self) -> DependencyList:
+        """
+        Check if all dependencies of a package are available for the target triple.
+        """
+
+        def _do_check(dep: Package):
+            if dep.basename not in PACKAGES:
+                raise PackageNotFoundError(dep.basename)
+
+            if PACKAGES[dep.basename].isany():
+                return True  # Any package can be used for any target
+
+            if self.context.target_triple not in PACKAGES[dep.basename].triples:
+                return False
+
+            return True
+
+        deplist = DependencyList([], [], [])
+
+        for dep in self.context.srcpkg.depends:
+            if not _do_check(dep.basepkg):
+                deplist.depends.append(dep)
+
+        for dep in self.context.srcpkg.makedepends:
+            if not _do_check(dep.basepkg):
+                deplist.makedepends.append(dep)
+
+        for dep in self.context.srcpkg.rebuild:
+            if not _do_check(dep.basepkg):
+                deplist.rebuild.append(dep)
+
+        return deplist
+
+    def execute(self):
+        print()
+        print("Now executing the porting steps:")
+
+        result = True
+        source_dir = self.context.srcpkg.pkgdir
+
+        def log_success():
+            cprint(f"OK  ", 'green', attrs=['bold'], end=' ', flush=True)
+
+        def log_failure():
+            cprint(f"FAIL", 'red', attrs=['bold', 'blink'], end=' ', flush=True)
+
+        # ensure we are in the correct directory
+        if not os.path.isdir(source_dir):
+            raise Exception(f"Directory '{source_dir}' does not exist, sanity check failed")
+
+        cprint("Steps:  ", 'blue', attrs=['bold'], end="")
+        cprint("".join([f"{i+1:<5}" for i in range(len(self.actions))]), "green", attrs=["bold"])
+        cprint("Result: ", "blue", attrs=['bold'], end="")
+
+        for action in self.actions:
+            sleep(random.uniform(0.01, 0.1))
+
+            try:
+                if action.execute(self.context):
+                    log_success()
+                else:
+                    raise ExecutionError("Action failed for unknown reason")
+            except ExecutionError as e:
+                result = False
+                log_failure()
+                print("\n")
+                cprint(f"Step #{self.actions.index(action)+1} Failed: {action}", 'red', attrs=['bold'])
+                cprint(e.message, 'red', attrs=['bold'])
+                break
+
+        print()
+        return result
